e0d99bcaeaca2fd002031804ee7183c934110e70
splatEric <mike.smith@camc-ltd.co.uk>
2012-12-05 17:13:15 +0000

    + work with the current naming convention for element init function

--- assets/js/module.js
+++ assets/js/module.js
@@ -1,6 +1,5 @@
 var dr_grade_et_class = 'Element_OphCiExamination_DRGrading';
 
-
 function gradeCalculator(_drawing) {
     var doodleArray = _drawing.doodleArray;
     
@@ -236,7 +235,9 @@ $(document).ready(function() {
 			
 			try {
 				var el_class = $(element).attr('data-element-type-class');
-				window[el_class + '_init']();
+				// work around to match the function name inits
+				
+				window[el_class.replace('Element_','') + '_init']();
 			} catch (err) {
 				// nothing to do here
 			}
@@ -268,7 +269,6 @@ $(document).ready(function() {
 	 * Remove a child element
 	 */
 	$('#active_elements').delegate('.active_child_elements .removeElement button', 'click', function(e) {
-		console.log('child remove');
 		var element = $(this).closest('.element');
 		removeElement(element, true);
 		e.preventDefault();
