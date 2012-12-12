672a7ddb06d12f1fd2d0a006db109e21fbd2f89d
Mark Wadham <mark.wadham@openeyes.org.uk>
2012-12-12 13:55:51 +0000

    removed accidentally committed line

--- controllers/DefaultController.php
+++ controllers/DefaultController.php
@@ -3,7 +3,6 @@
 class DefaultController extends BaseEventTypeController {
 
 	public function actionCreate() {
-		file_put_contents("/tmp/debug",print_r($_POST,true));
 		parent::actionCreate();
 	}
 
