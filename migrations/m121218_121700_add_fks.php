a6df43a930df7b848e8d19a87386e246b246d43e
Jamie Neil <jamie.neil@gmail.com>
2012-12-18 13:57:06 +0000

    Nullable FK (resolves OE-2514)

--- migrations/m121218_121700_add_fks.php
+++ migrations/m121218_121700_add_fks.php
@@ -3,9 +3,11 @@
 class m121218_121700_add_fks extends OEMigration {
 
 	public function up() {
+		
+		// Gonioscopy
 		foreach(array('gonio_sup' => 'description','gonio_tem' => 'description','gonio_nas' => 'description','gonio_inf' => 'description','van_herick' => 'van_herick') as $field => $table) {
 			foreach(array('left','right') as $side) {
-				$this->alterColumn('et_ophciexamination_gonioscopy', $side.'_'.$field.'_id', 'int(1) unsigned');
+				$this->alterColumn('et_ophciexamination_gonioscopy', $side.'_'.$field.'_id', 'int(10) unsigned');
 				foreach(Element_OphCiExamination_Gonioscopy::model()->findAll() as $model) {
 					if(!$model->{$side.'_'.$field}) {
 						$model->{$side.'_'.$field.'_id'} = null;
@@ -19,6 +21,12 @@ class m121218_121700_add_fks extends OEMigration {
 				);
 			}
 		}
+		
+		// Optic Disc
+		foreach(array('left','right') as $side) {
+			$this->alterColumn('et_ophciexamination_opticdisc', $side.'_cd_ratio_id', 'int(10) unsigned');
+		}
+		
 	}
 
 	public function down() {
