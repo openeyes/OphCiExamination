ddb9fa4d1789a33d1d68d6a88461da9379536042
Jamie Neil <jamie.neil@gmail.com>
2013-01-15 11:31:44 +0000

    Update dilation to new template design (incomplete)

--- views/default/form_Element_OphCiExamination_Dilation.php
+++ views/default/form_Element_OphCiExamination_Dilation.php
@@ -17,23 +17,12 @@
  * @license http://www.gnu.org/licenses/gpl-3.0.html The GNU General Public License V3.0
  */
 ?>
-<div class="element <?php echo $element->elementType->class_name ?>"
-	data-element-type-id="<?php echo $element->elementType->id ?>"
-	data-element-type-class="<?php echo $element->elementType->class_name ?>"
-	data-element-type-name="<?php echo $element->elementType->name ?>"
-	data-element-display-order="<?php echo $element->elementType->display_order ?>">
-	<div class="removeElement">
-		<button class="classy blue mini">
-			<span class="button-span icon-only"><img
-				src="<?php echo Yii::app()->createUrl('img/_elements/btns/mini-cross.png')?>"
-				alt="+" width="24" height="22"> </span>
-		</button>
-	</div>
-	<h4 class="elementTypeName">
-		<?php  echo $element->elementType->name; ?>
-	</h4>
-	<div class="cols2 clearfix">
-		<div class="side left eventDetail" data-side="right">
+<div class="cols2 clearfix">
+	<?php echo $form->hiddenInput($element, 'eye_id', false, array('class' => 'sideField')); ?>
+	<div class="side left eventDetail<?php if(!$element->hasRight()) { ?> inactive<?php } ?>"
+		data-side="right">
+		<div class="activeForm">
+			<a href="#" class="removeSide">-</a>
 			<?php echo $form->dropDownListNoPost('dilation_drug_right',$element->getUnselectedDilationDrugs('right'),'',array('empty'=>'--- Please select ---'))?>
 			<button class="clearDilationRight classy green mini" type="button">
 				<span class="button-span button-span-green">Clear</span>
@@ -61,7 +50,14 @@
 				</table>
 			</div>
 		</div>
-		<div class="side right eventDetail" data-side="left">
+		<div class="inactiveForm">
+			<a href="#">Add right side</a>
+		</div>
+	</div>
+	<div class="side right eventDetail<?php if(!$element->hasLeft()) { ?> inactive<?php } ?>"
+		data-side="left">
+		<div class="activeForm">
+			<a href="#" class="removeSide">-</a>
 			<?php echo $form->dropDownListNoPost('dilation_drug_left',$element->getUnselectedDilationDrugs('left'),'',array('empty'=>'--- Please select ---'))?>
 			<button class="clearDilationLeft classy green mini" type="button">
 				<span class="button-span button-span-green">Clear</span>
@@ -88,6 +84,9 @@
 					</tbody>
 				</table>
 			</div>
+		</div>
+		<div class="inactiveForm">
+			<a href="#">Add left side</a>
 		</div>
 	</div>
 </div>
