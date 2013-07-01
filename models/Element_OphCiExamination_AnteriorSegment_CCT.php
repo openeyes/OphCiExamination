9d87a5cc737902703d162b81d606b53d123ead72
splatEric <mike.smith@camc-ltd.co.uk>
2013-07-01 11:10:34 +0100

    + added CMT and OCT elements + minor change to CCT validation

--- models/Element_OphCiExamination_AnteriorSegment_CCT.php
+++ models/Element_OphCiExamination_AnteriorSegment_CCT.php
@@ -59,8 +59,8 @@ class Element_OphCiExamination_AnteriorSegment_CCT extends SplitEventTypeElement
 				array('eye_id, event_id', 'safe'),
 				array('left_method_id ,left_value', 'requiredIfSide', 'side' => 'left'),
 				array('right_method_id, right_value', 'requiredIfSide', 'side' => 'right'),
-				array('left_value', 'numerical', 'integerOnly' => false, 'numberPattern' => '/^[0-9]{1,3}(\.[0-9])?$/', 'message' => 'Left Value must be 0.0 - 999.9'),
-				array('right_value', 'numerical', 'integerOnly' => false, 'numberPattern' => '/^[0-9]{1,3}(\.[0-9])?$/', 'message' => 'Right Value must be 0.0 - 999.9'),
+				array('left_value', 'numerical', 'integerOnly' => true, 'max' => 1000, 'min' => 0),
+				array('right_value', 'numerical', 'integerOnly' => true, 'max' => 1000, 'min' => 0),
 				// The following rule is used by search().
 				// Please remove those attributes that should not be searched.
 				array('id, event_id, left_method_id, right_method_id, left_value, right_value', 'safe', 'on' => 'search'),
@@ -119,6 +119,7 @@ class Element_OphCiExamination_AnteriorSegment_CCT extends SplitEventTypeElement
 		$criteria->compare('right_value', $this->right_value);
 		$criteria->compare('left_method_id', $this->left_method_id);
 		$criteria->compare('right_method_id', $this->right_method_id);
+		
 		
 		return new CActiveDataProvider(get_class($this), array(
 				'criteria' => $criteria,
