30a666491818c37d21f8d522dcd8292770de7074
Jamie Neil <jamie.neil@openeyes.org.uk>
2013-08-06 11:35:11 +0100

    Removed subspecialty specific data from migrations

--- migrations/m121111_155600_glaucoma.php
+++ migrations/m121111_155600_glaucoma.php
@@ -144,13 +144,14 @@ class m121111_155600_glaucoma extends OEMigration
 				'name' => 'Expert Mode',
 				'default_value' => 0,
 		));
-		$glaucoma_ss = Subspecialty::model()->find('ref_spec = \'GL\'');
-		$this->insert('setting_subspecialty', array(
-				'subspecialty_id' => $glaucoma_ss->id,
-				'element_type_id' => $element_types['Element_OphCiExamination_Gonioscopy']['id'],
-				'key' => 'expert',
-				'value' => 1
-		));
+		if($glaucoma_ss = Subspecialty::model()->find('ref_spec = \'GL\'')) {
+			$this->insert('setting_subspecialty', array(
+					'subspecialty_id' => $glaucoma_ss->id,
+					'element_type_id' => $element_types['Element_OphCiExamination_Gonioscopy']['id'],
+					'key' => 'expert',
+					'value' => 1
+				));
+		}
 
 		// Optic Disc
 		$this->addColumn('et_ophciexamination_opticdisc', 'eye_id', "int(10) unsigned NOT NULL DEFAULT $both_eyes_id");
@@ -184,13 +185,15 @@ class m121111_155600_glaucoma extends OEMigration
 					'element_type_id' => $element_type_id
 			));
 		}
-		$glaucoma_id = Subspecialty::model()->find("ref_spec = 'GL'")->id;
-		$parent_id = OphCiExamination_ElementSetRule::model()->find("clause = 'subspecialty_id'")->id;
-		$this->insert('ophciexamination_element_set_rule',array(
-				'set_id' => $set_id,
-				'parent_id' => $parent_id,
-				'value' => $glaucoma_id
-		));
+		if($glaucoma_ss) {
+			$glaucoma_id = Subspecialty::model()->find("ref_spec = 'GL'")->id;
+			$parent_id = OphCiExamination_ElementSetRule::model()->find("clause = 'subspecialty_id'")->id;
+			$this->insert('ophciexamination_element_set_rule',array(
+					'set_id' => $set_id,
+					'parent_id' => $parent_id,
+					'value' => $glaucoma_id
+				));
+		}
 
 		$migrations_path = dirname(__FILE__);
 		$this->initialiseData($migrations_path);
