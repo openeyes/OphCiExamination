185eeeaf89d6fb4c804df4b9aafbb39e07eab32b
Richard Willis <willis.rh@gmail.com>
2013-08-15 16:16:10 +0100

    Added default grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,4 +5,7 @@ module.exports = function(grunt) {
 
   /* Load the npm grunt tasks */
   require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+  /* Load our custom grunt tasks */
+  grunt.loadTasks('./grunt/tasks');
 };
