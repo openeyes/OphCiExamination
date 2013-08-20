304117d07b7a33b143b9bc68883bfb94373a2eb0
Richard Willis <willis.rh@gmail.com>
2013-08-20 15:33:23 +0100

    Removed warning when compiling Sass if the module does not exist in the OpenEyes application

--- grunt/config/compass.js
+++ grunt/config/compass.js
@@ -1,28 +1,4 @@
-var path = require('path');
-
 module.exports = function(grunt) {
-
-	// Get the path to the core 'sass' directory.
-	// We add this import path to the compass compilation so that we can import the
-	// core mixins, functions and variables.
-	var importPath = (function getimportPath() {
-
-		var paths = [
-			'..',
-			'..',
-			'..',
-			'sass',
-			'new'
-		];
-
-		if (!grunt.file.exists.apply(grunt.file, paths)) {
-			grunt.fail.warn('Unable to compile Sass as this module does not exist within the OpenEyes core application.');
-			return null;
-		}
-
-		return path.join.apply(path, paths);
-	}());
-
 	return {
 		dist: {
 			options: {
@@ -30,7 +6,7 @@ module.exports = function(grunt) {
 				cssDir: 'assets/css',
 				imagesDir: 'assets/img/new',
 				generatedImagesDir: 'assets/img/sprites',
-				importPath: importPath,
+				importPath: '../../../sass/new',
 				outputStyle: 'expanded',
 				relativeAssets: true,
 				httpPath: '',
