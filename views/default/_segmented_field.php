0237f804edd63fab1c27f3c0f613a53a8aba6381
Mark Wadham <mark.wadham@openeyes.org.uk>
2013-08-27 13:45:20 +0000

    [OE-3329] fixed a rendering bug in the _segmented_field view in the examination refraction element.

--- views/default/_segmented_field.php
+++ views/default/_segmented_field.php
@@ -19,5 +19,5 @@
 ?>
 <?php echo CHtml::dropDownList(get_class($element).'_'.$field.'_sign', ($element->$field > 0) ? 1 : -1, CHtml::listData(OphCiExamination_Refraction_Sign::model()->findAll(array('order'=>'display_order')), 'name', 'value'))?> 
 <?php echo CHtml::dropDownList(get_class($element).'_'.$field.'_integer', abs((int) $element->$field), CHtml::listData(OphCiExamination_Refraction_Integer::model()->findAll(array('order'=>'display_order')),'value','value'))?> 
-<?php echo CHtml::dropDownList(get_class($element).'_'.$field.'_fraction', abs($element->$field) - (abs((int) $element->$field)), CHtml::listData(OphCiExamination_Refraction_Fraction::model()->findAll(array('order'=>'display_order')),'name','value'))?>
+<?php echo CHtml::dropDownList(get_class($element).'_'.$field.'_fraction', number_format(abs($element->$field) - (abs((int) $element->$field)),2), CHtml::listData(OphCiExamination_Refraction_Fraction::model()->findAll(array('order'=>'display_order')),'name','value'))?>
 <?php echo CHtml::activeHiddenField($element, $field)?>
