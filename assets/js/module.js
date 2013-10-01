c5282272b1c467c85deca6313fb0773b026b48d3
splatEric <mike.smith@camc-ltd.co.uk>
2013-10-01 15:40:29 +0100

    + [SUP-43] change to javascript to support change to name of 'no retinopathy'

--- assets/js/module.js
+++ assets/js/module.js
@@ -49,7 +49,7 @@ function gradeCalculator(_drawing) {
     var maculopathy = "M0";
     var retinopathy_photocoagulation = false;
     var maculopathy_photocoagulation = false;
-    var clinical = "None";
+    var clinical = "No retinopathy";
     var dnv_within = false;
 
     // Get reference to PostPole doodle
