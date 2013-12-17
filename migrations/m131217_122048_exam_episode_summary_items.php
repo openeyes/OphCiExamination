b6258543c059ebc6ef4b99334b043d3f1db145fa
Ben Spencer <ben.spencer@openeyes.org.uk>
2013-12-17 14:22:52 +0000

    CCT and IOP episode summary items.

---
+++ migrations/m131217_122048_exam_episode_summary_items.php
@@ -0,0 +1,20 @@
+<?php
+
+class m131217_122048_exam_episode_summary_items extends OEMigration
+{
+	public function up()
+	{
+		$event_type_id = $this->dbConnection->createCommand()->select('id')->from('event_type')->where('class_name = ?', array('OphCiExamination'))->queryScalar();
+
+		$this->insert('episode_summary_item', array('event_type_id' => $event_type_id, 'name' => 'CCT'));
+		$this->insert('episode_summary_item', array('event_type_id' => $event_type_id, 'name' => 'IOP'));
+	}
+
+	public function down()
+	{
+		$event_type_id = $this->dbConnection->createCommand()->select('id')->from('event_type')->where('class_name = ?', array('OphCiExamination'))->queryScalar();
+
+		$this->delete('episode_summary_item', 'event_type_id = ? and name = ?', array($event_type_id, 'CCT'));
+		$this->delete('episode_summary_item', 'event_type_id = ? and name = ?', array($event_type_id, 'IOP'));
+	}
+}
