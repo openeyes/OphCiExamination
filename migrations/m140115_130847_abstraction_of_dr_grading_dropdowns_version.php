5d0f25a80e47b0c57a8eeae2ff808e7b620136ff
Mark <m@rkw.me>
2014-02-20 13:40:25 +0000

    [OE-3987] removed unique keys from version tables

--- migrations/m140115_130847_abstraction_of_dr_grading_dropdowns_version.php
+++ migrations/m140115_130847_abstraction_of_dr_grading_dropdowns_version.php
@@ -9,10 +9,10 @@ class m140115_130847_abstraction_of_dr_grading_dropdowns_version extends CDbMigr
 		$this->addColumn('ophciexamination_drgrading_nscmaculopathy_version','code','varchar(2) NOT NULL');
 		$this->addColumn('ophciexamination_drgrading_nscretinopathy_version','code','varchar(2) NOT NULL');
 
-		$this->createIndex('ophciexamination_drgrading_clinicalmaculopathy_version_code','ophciexamination_drgrading_clinicalmaculopathy_version','code',true);
-		$this->createIndex('ophciexamination_drgrading_clinicalretinopathy_version_code','ophciexamination_drgrading_clinicalretinopathy_version','code',true);
-		$this->createIndex('ophciexamination_drgrading_nscmaculopathy_version_code','ophciexamination_drgrading_nscmaculopathy_version','code',true);
-		$this->createIndex('ophciexamination_drgrading_nscretinopathy_version_code','ophciexamination_drgrading_nscretinopathy_version','code',true);
+		$this->createIndex('ophciexamination_drgrading_clinicalmaculopathy_version_code','ophciexamination_drgrading_clinicalmaculopathy_version','code');
+		$this->createIndex('ophciexamination_drgrading_clinicalretinopathy_version_code','ophciexamination_drgrading_clinicalretinopathy_version','code');
+		$this->createIndex('ophciexamination_drgrading_nscmaculopathy_version_code','ophciexamination_drgrading_nscmaculopathy_version','code');
+		$this->createIndex('ophciexamination_drgrading_nscretinopathy_version_code','ophciexamination_drgrading_nscretinopathy_version','code');
 	}
 
 	public function down()
