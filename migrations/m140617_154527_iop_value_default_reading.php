c54e250030fc9bb91f6ca814b6db340508b88253
Richard Willis <willis.rh@gmail.com>
2014-06-19 11:03:58 +0100

    [OE-4347] Don't use models in migrations

--- migrations/m140617_154527_iop_value_default_reading.php
+++ migrations/m140617_154527_iop_value_default_reading.php
@@ -4,21 +4,20 @@ class m140617_154527_iop_value_default_reading extends OEMigration
 {
 	public function up()
 	{
-		$model = \OEModule\OphCiExamination\models\OphCiExamination_IntraocularPressure_Reading::model();
-		$reading = $model->find('value = 17');
+		$reading = Yii::app()->db->createCommand("SELECT id FROM ophciexamination_intraocularpressure_reading WHERE value = 17")->queryRow();
 
 		$this->insert('setting_metadata', array(
 			'element_type_id' => $this->getIdOfElementTypeByClassName('OEModule\OphCiExamination\models\Element_OphCiExamination_IntraocularPressure'),
 			'field_type_id' => 2, // Dropdown
 			'key' => 'default_reading_id',
 			'name' => 'Default reading',
-			'default_value' => $reading->id
+			'default_value' => $reading['id']
 		));
 	}
 
 	public function down()
 	{
-		$id = $this->getIdOfElementTypeByClassName('OEModule\OphCiExamination\models\Element_OphCiExamination_IntraocularPressure');
-		$this->delete('setting_metadata', '`element_type_id` = '.$id.' AND `key` = \'default_reading_id\'');
+		$element_type_id = $this->getIdOfElementTypeByClassName('OEModule\OphCiExamination\models\Element_OphCiExamination_IntraocularPressure');
+		$this->delete('setting_metadata', '`element_type_id` = '.$element_type_id.' AND `key` = \'default_reading_id\'');
 	}
 }
