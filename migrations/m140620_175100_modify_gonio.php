3b3d2701698822d7c574673ed8099c8908d54aaa
Diego Gullo <diego_gullo@bizmate.biz>
2014-06-25 10:30:00 +0100

    [OE-4364] - fix migration to account for version table changes

--- migrations/m140620_175100_modify_gonio.php
+++ migrations/m140620_175100_modify_gonio.php
@@ -8,7 +8,9 @@ class m140620_175100_modify_gonio extends CDbMigration
 		$this->dropForeignKey('et_ophciexam_overallmanagementplan_rgonio_id_fk', 'et_ophciexamination_overallmanagementplan');
 
 		$this->dropColumn('et_ophciexamination_overallmanagementplan', 'right_gonio_id');
+		$this->dropColumn('et_ophciexamination_overallmanagementplan_version', 'right_gonio_id');
 		$this->renameColumn('et_ophciexamination_overallmanagementplan', 'left_gonio_id', 'gonio_id');
+		$this->renameColumn('et_ophciexamination_overallmanagementplan_version', 'left_gonio_id', 'gonio_id');
 		$this->addForeignKey('et_ophciexam_overallmanagementplan_gonio_id_fk', 'et_ophciexamination_overallmanagementplan',
 			'gonio_id', 'ophciexamination_visitinterval', 'id'
 		);
@@ -18,7 +20,9 @@ class m140620_175100_modify_gonio extends CDbMigration
 	{
 		$this->dropForeignKey('et_ophciexam_overallmanagementplan_gonio_id_fk', 'et_ophciexamination_overallmanagementplan');
 		$this->renameColumn('et_ophciexamination_overallmanagementplan', 'gonio_id', 'left_gonio_id');
+		$this->renameColumn('et_ophciexamination_overallmanagementplan_version', 'gonio_id', 'left_gonio_id');
 		$this->addColumn('et_ophciexamination_overallmanagementplan', 'right_gonio_id','int(11) DEFAULT NULL AFTER left_gonio_id');
+		$this->addColumn('et_ophciexamination_overallmanagementplan_version', 'right_gonio_id','int(11) DEFAULT NULL AFTER left_gonio_id');
 		$this->addForeignKey('et_ophciexam_overallmanagementplan_lgonio_id_fk', 'et_ophciexamination_overallmanagementplan',
 			'left_gonio_id', 'ophciexamination_visitinterval', 'id'
 		);
