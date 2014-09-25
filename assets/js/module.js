09fb2bfa4c986981ca6875e99781a77504ceca00
Diego Gullo <diego_gullo@bizmate.biz>
2014-09-25 15:17:01 +0100

    [OE-4755] - modify form and script to allow ordered re-rendering of Dilation drugs

--- assets/js/module.js
+++ assets/js/module.js
@@ -900,11 +900,12 @@ $(document).ready(function() {
 		var wrapper = $(this).closest('.side');
 		var side = wrapper.attr('data-side');
 		var row = $(this).closest('tr');
+		var data_order = row.attr('data-order');
 		var id = $('.drugId', row).val();
 		var name = $('.drugName', row).text();
 		row.remove();
 		var dilation_drug = wrapper.find('.dilation_drug');
-		dilation_drug.append('<option value="'+id+'">'+name+'</option>');
+		dilation_drug.append('<option value="'+id+'" data-order="'+ data_order +'">'+name+'</option>');
 		sort_selectbox(dilation_drug);
 		if ($('.dilation_table tbody tr', wrapper).length == 0) {
 			$('.dilation_table', wrapper).hide();
@@ -1164,6 +1165,7 @@ function OphCiExamination_Dilation_getNextKey() {
 
 function OphCiExamination_Dilation_addTreatment(element, side) {
 	var drug_id = $('option:selected', element).val();
+	var data_order = $('option:selected', element).data('order');
 	if(drug_id) {
 		var drug_name = $('option:selected', element).text();
 		$('option:selected', element).remove();
@@ -1172,7 +1174,8 @@ function OphCiExamination_Dilation_addTreatment(element, side) {
 			"key" : OphCiExamination_Dilation_getNextKey(),
 			"side" : side,
 			"drug_name" : drug_name,
-			"drug_id" : drug_id
+			"drug_id" : drug_id,
+			"data_order" : data_order
 		};
 		var form = Mustache.render(template, data);
 		var table = $('#event-content .'+OE_MODEL_PREFIX+'Element_OphCiExamination_Dilation [data-side="' + side + '"] .dilation_table');
