938e30afea779903860413693ee3cd0156e21c2e
Mike Smith <mike.smith@openeyes.org.uk>
2014-10-13 13:24:31 +0100

    + fix VC shortcodes

--- migrations/m141006_133544_new_shortcodes.php
+++ migrations/m141006_133544_new_shortcodes.php
@@ -5,10 +5,10 @@ class m141006_133544_new_shortcodes extends  OEMigration
 	public function safeUp()
 	{
 		$event_type_id = $this->dbConnection->createCommand('select id from event_type where class_name = "OphCiExamination"')->queryScalar();
-		$this->insert('patient_shortcode', array('event_type_id' => $event_type_id, 'default_code' => 'inr', 'code' => 'inr', 'method' => 'getIOPReadingRightNoUnits', 'description' => 'Intraocular pressure, right eye reading no units'));
-		$this->insert('patient_shortcode', array('event_type_id' => $event_type_id, 'default_code' => 'inl', 'code' => 'inl', 'method' => 'getIOPReadingLeftNoUnits', 'description' => 'Intraocular pressure, left eye reading no units'));
-		$this->insert('patient_shortcode', array('event_type_id' => $event_type_id, 'default_code' => 'inr', 'code' => 'cnr', 'method' => 'getCCTRightNoUnits', 'description' => 'Central corneal thickness, right eye reading no units'));
-		$this->insert('patient_shortcode', array('event_type_id' => $event_type_id, 'default_code' => 'inl', 'code' => 'cnl', 'method' => 'getCCTLeftNoUnits', 'description' => 'Central corneal thickness, left eye reading no units'));
+		$this->insert('patient_shortcode', array('event_type_id' => $event_type_id, 'default_code' => 'ior', 'code' => 'ior', 'method' => 'getIOPReadingRightNoUnits', 'description' => 'Intraocular pressure, right eye reading no units'));
+		$this->insert('patient_shortcode', array('event_type_id' => $event_type_id, 'default_code' => 'iol', 'code' => 'iol', 'method' => 'getIOPReadingLeftNoUnits', 'description' => 'Intraocular pressure, left eye reading no units'));
+		$this->insert('patient_shortcode', array('event_type_id' => $event_type_id, 'default_code' => 'cnr', 'code' => 'cnr', 'method' => 'getCCTRightNoUnits', 'description' => 'Central corneal thickness, right eye reading no units'));
+		$this->insert('patient_shortcode', array('event_type_id' => $event_type_id, 'default_code' => 'cnl', 'code' => 'cnl', 'method' => 'getCCTLeftNoUnits', 'description' => 'Central corneal thickness, left eye reading no units'));
 	}
 
 	public function safeDown()
