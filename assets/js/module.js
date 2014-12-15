b1b3293683296f062fed409b04a6646dc3550805
Ben Spencer <ben.spencer@openeyes.org.uk>
2014-12-15 11:07:41 +0000

    Remove debug.

--- assets/js/module.js
+++ assets/js/module.js
@@ -809,7 +809,6 @@ $(document).ready(function() {
 
 	$(this).delegate('.addReading', 'click', function(e) {
 		var side = $(this).closest('.side').attr('data-side');
-        console.log('side ' + side);
 		OphCiExamination_VisualAcuity_addReading(side);
 		// VA can affect DR
 		OphCiExamination_DRGrading_update(side);
@@ -1390,13 +1389,11 @@ function OphCiExamination_VisualAcuity_getNextKey() {
 }
 
 function OphCiExamination_VisualAcuity_addReading(side) {
-    console.log('Add reading: with side ' + side)
 	var template = $('#visualacuity_reading_template').html();
 	var data = {
 		"key" : OphCiExamination_VisualAcuity_getNextKey(),
 		"side" : side
 	};
-    console.log('Add reading: data ' + data + ' template ' + template)
 	var form = Mustache.render(template, data);
 
 	$('section[data-element-type-class="'+OE_MODEL_PREFIX+'Element_OphCiExamination_VisualAcuity"] .element-eye.'+side+'-eye .noReadings').hide().find('input:checkbox').each(function() {
@@ -1580,7 +1577,6 @@ function OphCiExamination_DRGrading_canUpdate(side) {
  * @param side
  */
 function OphCiExamination_DRGrading_update(side) {
-    console.log('DR Grading update with side ' + side + ' model prefix ' + OE_MODEL_PREFIX)
 	var physical_side = 'left';
 	if (side == 'left') {
 		physical_side = 'right';
