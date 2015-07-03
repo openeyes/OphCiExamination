567219b6d1bf528ba6868a63f71106bbc1a269ab
himanshupatel79 <hp4137@gmail.com>
2015-07-03 12:49:38 +0100

    OE-5616 :: Removed House Officer from grade

--- views/default/form_Element_OphCiExamination_PCRRisk.php
+++ views/default/form_Element_OphCiExamination_PCRRisk.php
@@ -251,7 +251,7 @@ if(isset($patientId))
 			</div>
 			<div class="large-2 column">
 				<?php echo CHtml::dropDownList('doctor_grade_id', 'doctor_grade_id',
-					CHtml::listData(DoctorGrade::model()->findAll(array('order' => 'display_order')), 'id', 'grade'),
+					CHtml::listData(DoctorGrade::model()->findAll($criteria->condition = "grade != 'House officer'", array('order' => 'display_order')), 'id', 'grade'),
 					array(
 						'empty' => '- Select Doctor Grade -',
 						'options' => array($pcr['doctor_grade_id'] => array('selected' => true))
