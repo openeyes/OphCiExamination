bd4b72307f0627d1fc3a1b5dfc9157f812b081d5
pgwanz <pgwanz@hotmail.com>
2015-10-15 13:14:49 +0100

    adding new genders to database

---
+++ migrations/m151015_110043_add_new_genders.php
@@ -0,0 +1,33 @@
+<?php
+
+class m151015_110043_add_new_genders extends CDbMigration
+{
+	protected $genders = array('Unknown', 'Other');
+	public function up()
+	{
+		foreach($this->genders as $gender)
+		{
+			$this->insert('gender', $gender);
+		}
+
+    }
+
+	public function down()
+	{
+		foreach($this->genders as $gender)
+		{
+			$this->delete('gender', "name =" . $gender);
+		}
+	}
+
+	/*
+	// Use safeUp/safeDown to do migration with transaction
+	public function safeUp()
+	{
+	}
+
+	public function safeDown()
+	{
+	}
+	*/
+}
